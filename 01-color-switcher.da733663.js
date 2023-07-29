766207e5e195292dd7aeb0bbe52bab57fa08b4b2
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2023-07-29 16:14:35 +0000

    Deploying to gh-pages from @ ServerIbraimovUa/goit-js-hw-09@f1d309297d939980e7e302adcffddf8cbcea85ea 🚀

--- 01-color-switcher.da733663.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");let d;a.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,d=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),a.addEventListener("click",(function(){clearInterval(d)}));
-//# sourceMappingURL=01-color-switcher.da733663.js.map
